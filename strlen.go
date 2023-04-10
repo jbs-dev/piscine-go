2e7e8de3e57c5567603c44f2e62fd91fadc1656f
jsmith <jbsqlt@gmail.com>
2023-04-10 12:55:39 +0100

    changes

--- strlen.go
+++ strlen.go
@@ -2,13 +2,9 @@ package piscine
 
 // StrLen returns the length of a string, which is the count of characters in the string.
 func StrLen(s string) int {
-	// Convert the input string to a slice of runes. A rune represents a Unicode code point,
-	// which can be a single character or a sequence of bytes for multibyte characters.
-	// This conversion ensures that the length calculation will be accurate for strings
-	// containing multibyte characters.
-	runeSlice := []rune(s)
-
-	// Use the built-in len function to determine the length of the rune slice, which
-	// corresponds to the number of characters in the input string.
+	runeSlice := []rune(s) // Convert the input string to a slice of runes.
 	return len(runeSlice)
 }
+
+// Use the built-in len function to determine the length of the rune slice, which
+// corresponds to the number of characters in the input string.
