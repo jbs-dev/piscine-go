2eac536656a33c9f2f149827f967063b88f2bcba
jsmith <jbsqlt@gmail.com>
2023-04-10 12:53:47 +0100

    changes

--- isupper.go
+++ isupper.go
@@ -3,18 +3,12 @@ package piscine
 // IsUpper returns true if all characters in 'str' are uppercase letters (A-Z),
 // and false otherwise.
 func IsUpper(str string) bool {
-	// Count the number of runes in 'str'
-	runeCount := arrayCount(str)
-	// Initialize a variable to count the number of uppercase letters in 'str'
-	count := 0
-	// Iterate over each rune in 'str'
-	for _, i := range str {
-		// If the rune is an uppercase letter, increment the count
-		if i >= 'A' && i <= 'Z' {
+	runeCount := arrayCount(str) // Count the number of runes in 'str'
+	count := 0                   // Initialize a variable to count the number of uppercase letters in 'str'
+	for _, i := range str {      // Iterate over each rune in 'str'
+		if i >= 'A' && i <= 'Z' { // If the rune is an uppercase letter, increment the count
 			count++
 		}
 	}
-	// Return true if the number of uppercase letters is equal to the number of runes in 'str',
-	// and false otherwise
-	return count == runeCount
+	return count == runeCount // Return true if the number of uppercase letters is equal to the number of runes in 'str'
 }
